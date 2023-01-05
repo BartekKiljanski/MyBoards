5c455e5364059dbc8a103e77fa392f4d28387af3
Bartłomiej Kiljański <bartlomiej.kiljanski@aplitt.pl>
2023-01-05 23:55:05 +0100

    test

--- MyBoards/Entities/WorkItem.cs
+++ MyBoards/Entities/WorkItem.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyBoards.Entities
 {
@@ -6,19 +8,33 @@ namespace MyBoards.Entities
     {
       
         public int Id { get; set; }
+
         public string State { get; set; }
+        
         public string Area { get; set; }
+       
         public string IterationPath { get; set; }
-        public int Priority { get; set; }
+        public int Priority { get; set; } 
         // Epic
         public DateTime? StartDate { get; set; }
+
         public DateTime? EndDate { get; set; }
         // Issue
+       
         public decimal Efford { get; set; }
         // Task
+
         public string Activity { get; set; }
+ 
         public decimal RemaningWork { get; set; }
 
         public string Type { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public  User Author { get; set; }
+        public Guid AuthorId { get; set; }
+        public List<Tag> Tags { get; set; }
+        //public List<WorkItemTag> WorkItemTags { get; set; } = new List<WorkItemTag>();
     }
 }
